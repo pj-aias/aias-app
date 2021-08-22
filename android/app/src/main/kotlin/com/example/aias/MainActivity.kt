d55b6249166a6339c26bd9929041105d0353e80b
shunta nakajima <nakg_asimo@icloud.com>
2021-08-22 21:09:56 +0900

    remove all files

--- android/app/src/main/kotlin/com/example/aias/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.aias
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/SplashPage/viewmodel.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class SplashViewModel extends ChangeNotifier {
-  @override
-  void dispose() {
-    super.dispose();
-  }
-}

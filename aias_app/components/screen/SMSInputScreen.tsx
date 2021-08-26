8da9c2e067e06ace79f277abe7b2ca953312e99b
shunta nakajima <nakg_asimo@icloud.com>
2021-08-26 22:59:14 +0900

    add register/login screen

---
+++ aias_app/components/screen/SMSInputScreen.tsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { TextInput, SafeAreaView, StyleSheet, Button, Alert } from 'react-native';
+import { Text, View } from 'react-native';
+interface SMSInputScreenState {
+  codeText: string;
+}
+
+export class SMSInputScreen extends Component<{}, SMSInputScreenState> {
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      codeText: ""
+    }
+
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  private handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({codeText: e.target.value});
+  }
+
+  private handleSubmit = () => {
+    Alert.alert('this is you phone number: ' + this.state.codeText);
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text>Enter phone number to signin/signup</Text>
+          <TextInput
+            style={styles.textinput}
+            value={this.state.codeText} 
+            onChangeText={(text) => this.setState({codeText: text})} 
+          />
+          <Button
+            onPress={this.handleSubmit}
+            title="Send"
+            color="#841584"
+          />
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 100,
+    display: 'flex',
+    alignItems: 'center'
+  },
+  textinput: {
+    height: 30,
+    width: 200,
+    backgroundColor: 'white',
+    marginTop: 10
+  },
+  red: {
+    color: 'red',
+  },
+});

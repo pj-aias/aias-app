5a74025e75638800c017c8885dd7e7c8db32f496
akakou <akakou571@gmail.com>
2021-09-10 21:06:38 +0900

    [update] Extract app new

--- aias_app/util/router.ts
+++
@@ -1,7 +0,0 @@
-export const Router = {
-  OnbordingScreen: 'Onbording',
-  SMSInputScreen: 'SignUp/In',
-  SMSVarifyScreen: 'Varify',
-  OpnerScreen: 'Opner',
-} as const;
-export type Router = typeof Router[keyof typeof Router];

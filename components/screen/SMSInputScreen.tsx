72c6e5f490f0ce83d78f6131d5c9668afbb2bb50
akakou <akakou571@gmail.com>
2021-09-10 23:46:37 +0900

    [add] Communicate with issuer

--- components/screen/SMSInputScreen.tsx
+++ components/screen/SMSInputScreen.tsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import { TextInput, SafeAreaView, StyleSheet, Button, Alert } from 'react-native';
+import { TextInput, SafeAreaView, StyleSheet, Button, Alert, NativeSyntheticEvent, TextInputChangeEventData } from 'react-native';
 import { Text, View } from 'react-native';
 import { NavigationParams, NavigationScreenProp } from 'react-navigation';
 import { NavigationState } from '@react-navigation/native';
 import { Router } from '../../util/router';
+import Tor from 'react-native-tor';
+
+export type RequestHeaders = { [header: string]: string } | {};
+
+const ISSUER_URL = "http://2jz6o2vhsfb55tk2lv73gauoxjmh2uisra65umzkg22yq67mzkvg6ayd.onion/send_code";
 
 interface State {
-  codeText: string;
+  phoneNumber: string;
 }
 
 interface Props {
@@ -17,18 +22,34 @@ export class SMSInputScreen extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      codeText: '',
+      phoneNumber: '',
     };
     this.handleOnChange = this.handleOnChange.bind(this);
   }
 
-  private handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ codeText: e.target.value });
+  private handleOnChange = (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+    this.setState({ phoneNumber: e.nativeEvent.text });
   };
 
-  private handleSubmit = () => {
+  private handleSubmit = async () => {
     //request verify
-    this.props.navigation.navigate(Router.SMSVerifyScreen, {});
+    const tor = Tor();
+    await tor.startIfNotStarted();
+
+    const body = JSON.stringify({ phone_number: this.state.phoneNumber });
+
+    try {
+      await tor.post(ISSUER_URL, body, { 'Content-Type': 'text/json' }).then(resp => {
+        const setCookie = resp.headers["set-cookie"];
+        console.log(`result: ${setCookie}`);
+
+        this.props.navigation.navigate(Router.SMSVerifyScreen, { cookie: setCookie });
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    tor.stopIfRunning();
   };
 
   render() {
@@ -37,8 +58,8 @@ export class SMSInputScreen extends Component<Props, State> {
         <Text>Enter phone number to signin/signup</Text>
         <TextInput
           style={styles.textinput}
-          value={this.state.codeText}
-          onChangeText={text => this.setState({ codeText: text })}
+          value={this.state.phoneNumber}
+          onChangeText={text => this.setState({ phoneNumber: text })}
         />
         <Button onPress={this.handleSubmit} title="Send" color="#841584" />
       </SafeAreaView>
--- components/screen/SMSVerifyScreen.tsx
+++ components/screen/SMSVerifyScreen.tsx
@@ -1,25 +1,53 @@
+import { DEFAULT_EXTENSIONS } from '@babel/core';
 import React, { Component } from 'react';
-import { TextInput, SafeAreaView, StyleSheet, Button, Alert } from 'react-native';
+import { TextInput, SafeAreaView, StyleSheet, Button, Alert, NativeSyntheticEvent, TextInputChangeEventData, BackHandler } from 'react-native';
 import { Text, View } from 'react-native';
+import Tor from 'react-native-tor';
+
+const ISSUER_URL = "http://2jz6o2vhsfb55tk2lv73gauoxjmh2uisra65umzkg22yq67mzkvg6ayd.onion/verify_code";
+
 interface SMSVerifyScreenState {
-  codeText: string;
+  code: string;
 }
 
-export class SMSVerifyScreen extends Component<{}, SMSVerifyScreenState> {
-  constructor(props: {}) {
+interface SMSVerifyScreenProps {
+  cookie: string;
+}
+
+export class SMSVerifyScreen extends Component<SMSVerifyScreenProps, SMSVerifyScreenState> {
+  constructor(props: SMSVerifyScreenProps) {
     super(props);
     this.state = {
-      codeText: '',
+      code: '',
     };
 
     this.handleOnChange = this.handleOnChange.bind(this);
   }
 
-  private handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ codeText: e.target.value });
+  private handleOnChange = (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+    this.setState({ code: e.nativeEvent.text });
   };
 
-  private handleSubmit = () => { };
+  private handleSubmit = async () => {
+    const tor = Tor();
+    await tor.startIfNotStarted();
+
+    const body = JSON.stringify({ code: this.state.code, pubkey: "hoge" });
+    const headers = { 'Content-Type': 'text/json', "Cookie": this.props.cookie };
+
+    try {
+      await tor.post(ISSUER_URL, body, headers).then(resp => {
+        console.log(`code: ${resp.respCode}`);
+
+        tor.stopIfRunning();
+        BackHandler.exitApp();
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    tor.stopIfRunning();
+  };
 
   render() {
     return (
@@ -27,8 +55,8 @@ export class SMSVerifyScreen extends Component<{}, SMSVerifyScreenState> {
         <Text>verify code</Text>
         <TextInput
           style={styles.textinput}
-          value={this.state.codeText}
-          onChangeText={text => this.setState({ codeText: text })}
+          value={this.state.code}
+          onChangeText={text => this.setState({ code: text })}
         />
         <Button onPress={this.handleSubmit} title="Send" color="#841584" />
       </SafeAreaView>

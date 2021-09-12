c0f0cc319b888ad35a2c82ccf4592ad54af92807
akakou <akakou571@gmail.com>
2021-09-12 21:38:12 +0900

    [add] Set custom scheme

--- App.tsx
+++ App.tsx
@@ -7,30 +7,41 @@ import { SMSVerifyScreen } from './components/screen/SMSVerifyScreen';
 import { Router } from './util/router';
 import { OpnerScreen } from './components/screen/OpnerScreen';
 import { TestTorScreen } from './components/screen/TestTorScreen';
+import { Text } from 'react-native';
 
 const Stack = createStackNavigator();
+const config = {
+  screens: {
+    [Router.OpnerScreen]: 'key_gen',
+  },
+};
+
+const linking = {
+  prefixes: ['https://aias.com', 'aias://'],
+  config,
+};
 
 export default class App extends Component {
   render() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName={Router.SMSInputScreen}>
-          <Stack.Screen
-            name={Router.OnbordingScreen}
-            component={OnbordingScreen}
-          />
-          <Stack.Screen
-            name={Router.SMSInputScreen}
-            component={SMSInputScreen}
-          />
-          <Stack.Screen
-            name={Router.SMSVerifyScreen}
-            component={SMSVerifyScreen}
-          />
-          <Stack.Screen name={Router.OpnerScreen} component={OpnerScreen} />
-          <Stack.Screen name={Router.TestTorScreen} component={TestTorScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+    return (<NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
+      <Stack.Navigator>
+        <Stack.Screen
+          name={Router.SMSInputScreen}
+          component={SMSInputScreen}
+        />
+        <Stack.Screen
+          name={Router.SMSVerifyScreen}
+          component={SMSVerifyScreen}
+        />
+        <Stack.Screen
+          name={Router.OnbordingScreen}
+          component={OnbordingScreen}
+        />
+
+        <Stack.Screen name={Router.OpnerScreen} component={OpnerScreen} />
+        <Stack.Screen name={Router.TestTorScreen} component={TestTorScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
     )
   }
 }
